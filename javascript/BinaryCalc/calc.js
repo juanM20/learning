ca76ed1c3f5d77f7b1416d91e86a9c59f5381e4b
Juan Tellez <jmtp12poke@gmail.com>
2019-08-09 09:44:55 -0500

    dgrfjdg

--- javascript/BinaryCalc/calc.js
+++ javascript/BinaryCalc/calc.js
@@ -11,12 +11,57 @@ function Write_in_Res(e) {
     document.getElementById('res').innerHTML += num;
 }
 
-function Operate(operate_string) {
+function Operate() {
 
+    let operate_string = str.innerHTML;
     let num1;
     let num2;
+    let result;
+    let result2;
+    let index = 0;
 
-    operate_string.search("");
+    for (let i = 0; i < operate_string.length; i++) {
+        if (operate_string[i] == "+" || operate_string[i] == "-" || operate_string[i] == "*" || operate_string[i] == "/")
+            index = i;
+    }
+
+    num1 = operate_string.substr(0, index);
+    num2 = operate_string.substr(index + 1, operate_string.length - 1);
+
+    console.log('Num1: ' + parseInt(num1, 2));
+    console.log('Num2: ' + parseInt(num2, 2));
+
+    switch (operate_string[index]) {
+        case '+':
+            result = parseInt(num1, 2) + parseInt(num2, 2);
+            console.log('Decimal_Result: ' + result);
+            result2 = result.toString(2);
+            console.log('Binary_Result: ' + result2);
+            document.getElementById('res').innerHTML = result2;
+            break;
+        case '-':
+            result = parseInt(num1, 2) - parseInt(num2, 2);
+            console.log('Decimal_Result: ' + result);
+            result2 = result.toString(2);
+            console.log('Binary_Result: ' + result2);
+            document.getElementById('res').innerHTML = result2;
+            break;
+        case '*':
+            result = parseInt(num1, 2) * parseInt(num2, 2);
+            console.log('Decimal_Result: ' + result);
+            result2 = result.toString(2);
+            console.log('Binary_Result: ' + result2);
+            document.getElementById('res').innerHTML = result2;
+            break;
+        case '/':
+            result = parseInt(num1, 2) / parseInt(num2, 2);
+            console.log('Decimal_Result: ' + result);
+            result2 = result.toString(2);
+            console.log('Binary_Result: ' + result2);
+            document.getElementById('res').innerHTML = result2;
+            break;
+    }
+
 }
 
 
@@ -30,4 +75,4 @@ document.getElementById("btnMul").addEventListener("click", Write_in_Res);
 document.getElementById("btnDiv").addEventListener("click", Write_in_Res);
 
 
-document.getElementById("btnEql").onclick = Operate(str.innerText);
+document.getElementById("btnEql").addEventListener("click", Operate);
